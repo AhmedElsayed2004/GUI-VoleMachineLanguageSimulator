260f3953fd0c4031e9360014eb2fbacc2e7e6a26
Yousef Mostafa Farouk <yousefmostafafarouq@gmail.com>
2023-11-14 00:37:09 +0200

    Added basic UI

--- GUI-VoleMachineLanguageSimulator/src/UI.cpp
+++ GUI-VoleMachineLanguageSimulator/src/UI.cpp
@@ -1,94 +1,104 @@
 #include "UI.h"
-
-UI::UI(HWND hWnd) 
+void UI::CreateRenderTarget()
 {
-    if (!CreateDeviceD3D(hWnd)) {
-        CleanupDeviceD3D();
-        throw std::runtime_error("Failed to initialize UI");
-    }
-
-    Initialize();
+    ID3D10Texture2D* pBackBuffer;
+    m_g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    m_g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &m_g_mainRenderTargetView);
+    pBackBuffer->Release();
 }
 
-UI::~UI() {
-    CleanupDeviceD3D();
-    ImGui_ImplDX10_Shutdown();
-    ImGui_ImplWin32_Shutdown();
-    ImGui::DestroyContext();
+void UI::CleanupRenderTarget()
+{
+    if (m_g_mainRenderTargetView) { m_g_mainRenderTargetView->Release(); m_g_mainRenderTargetView = nullptr; }
 }
 
-void UI::Initialize() {
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
+UI::UI(ID3D10Device* g_pd3dDevice, IDXGISwapChain* g_pSwapChain, UINT g_ResizeWidth, UINT g_ResizeHeight, ID3D10RenderTargetView* g_mainRenderTargetView)
+{
+    m_g_pd3dDevice = g_pd3dDevice;
+    m_g_pSwapChain = g_pSwapChain;
+    m_g_ResizeWidth = g_ResizeWidth;
+    m_g_ResizeHeight = g_ResizeHeight;
+    m_g_mainRenderTargetView = g_mainRenderTargetView;
+    
+}
+void UI::Run()
+{
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    bool done = false;
+        MSG msg;
+        while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
+        {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                done = true;
+        }
 
-    ImGui::StyleColorsDark();
+        // Handle window resize (we don't resize directly in the WM_SIZE handler)
+        if (m_g_ResizeWidth != 0 && m_g_ResizeHeight != 0)
+        {
+            CleanupRenderTarget();
+            m_g_pSwapChain->ResizeBuffers(0, m_g_ResizeWidth, m_g_ResizeHeight, DXGI_FORMAT_UNKNOWN, 0);
+            m_g_ResizeWidth = m_g_ResizeHeight = 0;
+            CreateRenderTarget();
+        }
 
-    ImGuiStyle& style = ImGui::GetStyle();
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-        style.WindowRounding = 0.0f;
-        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-    }
+        // Start the Dear ImGui frame
+        ImGui_ImplDX10_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
 
-    ImGui_ImplWin32_Init(hwnd);
-    ImGui_ImplDX10_Init(g_pd3dDevice);
-}
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 
-void UI::Render() {
-    // Handle window resize
-    if (g_ResizeWidth != 0 && g_ResizeHeight != 0) {
-        CleanupRenderTarget();
-        g_pSwapChain->ResizeBuffers(0, g_ResizeWidth, g_ResizeHeight, DXGI_FORMAT_UNKNOWN, 0);
-        g_ResizeWidth = g_ResizeHeight = 0;
-        CreateRenderTarget();
-    }
+        ImGui::SetNextWindowSize({ 800,400 });
+        if (ImGui::Begin("Table", NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize))
+        {
+            if (ImGui::BeginTable("Memory", 17, ImGuiTableFlags_NoHostExtendX | ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersH))
+            {
+                ImGui::TableSetupColumn("");
+                for (int i = 0; i < 16; ++i)
+                {
+                    char val[2];
+                    val[0] = i < 10 ? '0' + i : 'A' + (i - 10);
+                    val[1] = '\0';
+                    ImGui::TableSetupColumn(val);
+                }
+                ImGui::TableHeadersRow();
 
-    ImGui_ImplDX10_NewFrame();
-    ImGui_ImplWin32_NewFrame();
-    ImGui::NewFrame();
+                for (int row = 0; row < 16; row++)
+                {
+                    char val[2];
+                    val[0] = row < 10 ? '0' + row : 'A' + (row - 10);
+                    val[1] = '\0';
 
-    // ImGui rendering code here
+                    ImGui::TableNextRow();
+                    ImGui::TableSetColumnIndex(0);
+                    ImGui::Text(val);
+                    for (int column = 1; column < 17; column++)
+                    {
+                        ImGui::TableSetColumnIndex(column);
+                        ImGui::Text("%d %d", 0, 0);
+                    }
+                }
+                ImGui::EndTable();
+            }
+        }ImGui::End();
 
-    // Rendering
-    ImGui::Render();
-    const float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
-    g_pd3dDevice->OMSetRenderTargets(1, &g_mainRenderTargetView, nullptr);
-    g_pd3dDevice->ClearRenderTargetView(g_mainRenderTargetView, clear_color_with_alpha);
-    ImGui_ImplDX10_RenderDrawData(ImGui::GetDrawData());
 
-    // Update and Render additional Platform Windows
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-        ImGui::UpdatePlatformWindows();
-        ImGui::RenderPlatformWindowsDefault();
-    }
+        // Rendering
+        ImGui::Render();
+        const float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
+        m_g_pd3dDevice->OMSetRenderTargets(1, &m_g_mainRenderTargetView, nullptr);
+        m_g_pd3dDevice->ClearRenderTargetView(m_g_mainRenderTargetView, clear_color_with_alpha);
+        ImGui_ImplDX10_RenderDrawData(ImGui::GetDrawData());
 
-    g_pSwapChain->Present(1, 0); // Present with vsync
-    //g_pSwapChain->Present(0, 0); // Present without vsync
-}
+        // Update and Render additional Platform Windows
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+        }
 
-void UI::HandleMessages(UINT msg, WPARAM wParam, LPARAM lParam) {
-    // Message handling code here
-    if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wParam, lParam))
-        return;
-
-    switch (msg) {
-        // Handle specific messages here
-    case WM_SIZE:
-        if (wParam == SIZE_MINIMIZED)
-            return;
-        g_ResizeWidth = (UINT)LOWORD(lParam);
-        g_ResizeHeight = (UINT)HIWORD(lParam);
-        return;
-    case WM_SYSCOMMAND:
-        if ((wParam & 0xfff0) == SC_KEYMENU)
-            return;
-        break;
-    case WM_DESTROY:
-        ::PostQuitMessage(0);
-        return;
-    }
+        m_g_pSwapChain->Present(1, 0); // Present with vsync
 }
--- GUI-VoleMachineLanguageSimulator/src/main.cpp
+++ GUI-VoleMachineLanguageSimulator/src/main.cpp
@@ -1,7 +1,162 @@
 #include "Machine.h"
+#include "UI.h"
+
+static ID3D10Device* g_pd3dDevice = nullptr;
+static IDXGISwapChain* g_pSwapChain = nullptr;
+static UINT                     g_ResizeWidth = 0, g_ResizeHeight = 0;
+static ID3D10RenderTargetView* g_mainRenderTargetView = nullptr;
+
+// Forward declarations of helper functions
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 
 int main()
 {
-	Machine machine;
-	machine.Run();
+    WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
+    ::RegisterClassExW(&wc);
+    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Dear ImGui DirectX10 Example", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
+
+    // Initialize Direct3D
+    if (!CreateDeviceD3D(hwnd))
+    {
+        CleanupDeviceD3D();
+        ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+        return 1;
+    }
+
+    // Show the window
+    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+    ::UpdateWindow(hwnd);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+    //io.ConfigViewportsNoAutoMerge = true;
+    //io.ConfigViewportsNoTaskBarIcon = true;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX10_Init(g_pd3dDevice);
+
+    UI ui(g_pd3dDevice, g_pSwapChain, g_ResizeWidth, g_ResizeHeight, g_mainRenderTargetView);
+    // UI RUNNER
+    while (true)
+    {
+        ui.Run();
+    }
+
+
+    ImGui_ImplDX10_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+
+    CleanupDeviceD3D();
+    ::DestroyWindow(hwnd);
+    ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+}
+
+
+// Helper functions
+bool CreateDeviceD3D(HWND hWnd)
+{
+    // Setup swap chain
+    DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory(&sd, sizeof(sd));
+    sd.BufferCount = 2;
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    UINT createDeviceFlags = 0;
+    //createDeviceFlags |= D3D10_CREATE_DEVICE_DEBUG;
+    HRESULT res = D3D10CreateDeviceAndSwapChain(nullptr, D3D10_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags, D3D10_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice);
+    if (res == DXGI_ERROR_UNSUPPORTED) // Try high-performance WARP software driver if hardware is not available.
+        res = D3D10CreateDeviceAndSwapChain(nullptr, D3D10_DRIVER_TYPE_WARP, nullptr, createDeviceFlags, D3D10_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice);
+    if (res != S_OK)
+        return false;
+
+    CreateRenderTarget();
+    return true;
+}
+
+void CleanupDeviceD3D()
+{
+    CleanupRenderTarget();
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = nullptr; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
+}
+
+void CreateRenderTarget()
+{
+    ID3D10Texture2D* pBackBuffer;
+    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+    pBackBuffer->Release();
+}
+
+void CleanupRenderTarget()
+{
+    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+}
+
+// Forward declare message handler from imgui_impl_win32.cpp
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Win32 message handler
+// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (wParam == SIZE_MINIMIZED)
+            return 0;
+        g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
+        g_ResizeHeight = (UINT)HIWORD(lParam);
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        return 0;
+    }
+    return ::DefWindowProcW(hWnd, msg, wParam, lParam);
 }

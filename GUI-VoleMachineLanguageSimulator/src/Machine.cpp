164391bb64146790e0f12f79209db9770496a093
Ahmed Elsayed <ahmedelsayed12004@gmail.com>
2023-11-14 21:36:19 +0200

    add load new instruction button

--- GUI-VoleMachineLanguageSimulator/src/Machine.cpp
+++ GUI-VoleMachineLanguageSimulator/src/Machine.cpp
@@ -4,118 +4,87 @@
 #include <regex>
 
 
-void Machine::Run()
+void Machine::Run(int choice,char* instructions)
 {
-    int choice{};
-
-    ResetMachine();
-    while (!LoadNewProgram())
-        std::cout << "Please load a new program" << std::endl;
 
     cpu.programCounter = m_startAddress;
 
-    while (choice != 5)
+    //std::cout << "Enter 1 to load a new program" << std::endl;
+    //std::cout << "Enter 2 to execute next step" << std::endl;
+    //std::cout << "Enter 3 to execute entire program" << std::endl;
+    //std::cout << "Enter 4 to Display information about the register, PC, IR and memory" << std::endl;
+    //std::cout << "Enter 5 to exit program" << std::endl;
+
+    //std::cin >> choice;
+
+    //while (choice < 1 || choice > 5)
+    //{
+    //    std::cout << "Please enter a valid choice" << std::endl;
+    //    std::cin >> choice;
+    //}
+
+    switch (choice)
     {
-        std::cout << "Enter 1 to load a new program" << std::endl;
-        std::cout << "Enter 2 to execute next step" << std::endl;
-        std::cout << "Enter 3 to execute entire program" << std::endl;
-        std::cout << "Enter 4 to Display information about the register, PC, IR and memory" << std::endl;
-        std::cout << "Enter 5 to exit program" << std::endl;
+    case 1:
+        ResetMachine();
+        LoadNewProgram( instructions);
 
-        std::cin >> choice;
+        cpu.programCounter = m_startAddress;
 
-        while (choice < 1 || choice > 5)
+        break;
+    case 2:
+        /*cpu.FetchInstruction(m_memory);
+
+        if (cpu.IsValidInstruction() && !cpu.isHalt)
         {
-            std::cout << "Please enter a valid choice" << std::endl;
-            std::cin >> choice;
+            cpu.ExecuteInstruction(m_memory);
         }
-
-        switch (choice)
+        else if (cpu.isHalt)
         {
-        case 1:
-            ResetMachine();
-            LoadNewProgram();
-
-            cpu.programCounter = m_startAddress;
-
-            break;
-        case 2:
+            std::cout << "Program has halted" << std::endl;
+        }
+        else if (!cpu.IsValidInstruction())
+        {
+            std::cout << "Invalid Instruction at address " << cpu.programCounter << std::endl;
+        }*/
+        break;
+    case 3:
+        /*while (true)
+        {
             cpu.FetchInstruction(m_memory);
-
             if (cpu.IsValidInstruction() && !cpu.isHalt)
             {
                 cpu.ExecuteInstruction(m_memory);
             }
+
             else if (cpu.isHalt)
             {
                 std::cout << "Program has halted" << std::endl;
+                break;
             }
+
             else if (!cpu.IsValidInstruction())
             {
                 std::cout << "Invalid Instruction at address " << cpu.programCounter << std::endl;
+                break;
             }
-            break;
-        case 3:
-            while (true)
-            {
-                cpu.FetchInstruction(m_memory);
-                if (cpu.IsValidInstruction() && !cpu.isHalt)
-                {
-                    cpu.ExecuteInstruction(m_memory);
-                }
-
-                else if (cpu.isHalt)
-                {
-                    std::cout << "Program has halted" << std::endl;
-                    break;
-                }
-
-                else if (!cpu.IsValidInstruction())
-                {
-                    std::cout << "Invalid Instruction at address " << cpu.programCounter << std::endl;
-                    break;
-                }
-            }
-            break;
-        case 4:
-            DisplayInfo();
-            break;
-        case 5:
-            break;
-        }
+        }*/
+        break;
+    case 4:
+        //DisplayInfo();
+        break;
+    case 5:
+        break;
     }
 }
 
-bool Machine::LoadNewProgram()
+bool Machine::LoadNewProgram(char* instructions)
 {
-	std::string fileName;
-
-	std::cout << "Enter a file"<< std::endl;
-	std::cin >> fileName;
-
-    m_inputFile.open(fileName);
-
-    // Check if the file has been opened successfully
-    if (!m_inputFile.is_open()) 
+    for (int i = 0;i < 1000;i+=2)
     {
-        std::cout << "Error opening the file" << std::endl;
-        return false;
+        if (instructions[i] == 0|| instructions[i+1] == 0) break;
+        m_loadedInstructions.push_back({ instructions[i],instructions[i + 1] });
     }
-    else
-    {
-        if (LoadAllInstructions())
-        {
-            std::cout << "Enter the start address" << std::endl;
-
-            while (!SetStartAddress())
-                std::cout << "Instructions don't fit inside of the machine, Please enter another start address" << std::endl;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
     LoadInstructionsIntoMemory();
 
     return true;
@@ -185,112 +154,9 @@ bool Machine::SetStartAddress()
     return true;
 }
 
-void Machine::DisplayInfo()
-{
-    std::cout << "        CPU        " << std::endl;
-    for (int i = 0; i < 18; i++)
-    {
-        if (i > 0 && i < 11)
-        {
-            if (i == 8)
-            {
-                std::cout << "|  " << 'R' << i - 1 << "  " << cpu.cpuRegister[i - 1].nibble[0]
-                    << cpu.cpuRegister[i - 1].nibble[1] << "  PC  " << cpu.programCounter;
-
-                // Calculate the number of digits
-                int NumOfDigits;
-                if (cpu.programCounter == 0)
-                {
-                    NumOfDigits = 1;
-                }
-                else
-                {
-                    NumOfDigits = log10(cpu.programCounter) + 1;
-                }
-
-                for (int i = 0; i < 5 - NumOfDigits; i++) {
-                    std::cout << " ";
-                }
-
-                std::cout << '|' << std::endl;
-
-                continue;
-            }
-            else if (i == 9)
-            {
-                std::cout << "|  " << 'R' << i - 1 << "  " << cpu.cpuRegister[i - 1].nibble[0]
-                    << cpu.cpuRegister[i - 1].nibble[1] << "  IR  " << cpu.IR[0] << cpu.IR[1] << cpu.IR[2] << cpu.IR[3] << " |" << std::endl;
-
-                continue;
-            }
-            std::cout << "|  " << 'R' << i - 1 << "  " << cpu.cpuRegister[i - 1].nibble[0] 
-                << cpu.cpuRegister[i - 1].nibble[1] << "           |" << std::endl;
-        }
-        else if (i > 10 && i < 17)
-        {
-            std::cout << "|  " << 'R' << char((i - 1) - 10 + 'A') << "  " << cpu.cpuRegister[i - 1].nibble[0] 
-                << cpu.cpuRegister[i - 1].nibble[1] << "           |" << std::endl;
-        }
-        else
-        {
-            std::cout << "---------------------" << std::endl;
-        }
-    }
-	
-    for (int i = 0; i < 35; i++)
-    {
-        std::cout << " ";
-    }
-    std::cout << "Main Memory";
-    for (int i = 0; i < 35; i++)
-    {
-        std::cout << " ";
-    }
-    
-    std::cout << std::endl;
-
-    for (int i = 0; i < 90; i++)
-    {
-        std::cout << "-";
-    }
-
-    std::cout << std::endl;
-    
-    std::cout << "|      ";
-    for (int i = 0; i < 16; i++)
-    {
-        if (i < 10)
-            std::cout << i << "    ";
-        else
-            std::cout << char('A' + (i - 10)) << "    ";
-    }
-    
-    std::cout << "  |" << std::endl;
-
-    for (int i = 0; i < 16; i++)
-    {
-        if (i < 10)
-            std::cout << "|  " << i << "   ";
-        else
-            std::cout << "|  " << char('A' + (i - 10)) << "   ";
-
-        for (int j = i * 16; j < (i + 1) * 16; j++)
-        {
-            std::cout << m_memory[j].nibble[0] << m_memory[j].nibble[1] << "   ";
-        }
-        std::cout << "  |" << std::endl;
-    }
-
-    for (int i = 0; i < 90; i++)
-    {
-        std::cout << "-";
-    }
-    
-    std::cout << std::endl;
-}
-
 void Machine::ResetMachine()
 {
+    m_loadedInstructions.clear();
     for (int i = 0; i < 256; ++i)
         m_memory[i] = { {'0','0'} };
 

3f7e680662fb46aab255c0d3c6a169ff7c3f90be
Yousef Mostafa Farouk <yousefmostafafarouq@gmail.com>
2023-11-25 20:35:58 +0200

    Removed uneeeded files

--- GUI-VoleMachineLanguageSimulator/src/main.cpp
+++ GUI-VoleMachineLanguageSimulator/src/main.cpp
@@ -11,9 +11,9 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 int main()
 {
-    WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
+    WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"Vole Machine Simulator", nullptr };
     ::RegisterClassExW(&wc);
-    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Dear ImGui DirectX10 Example", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
+    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Vole Machine Simulator", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
 
     UI ui(g_pd3dDevice, g_pSwapChain, g_ResizeWidth, g_ResizeHeight, g_mainRenderTargetView, hwnd, wc);
 
